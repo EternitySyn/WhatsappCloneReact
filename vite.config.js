2753e7a55077f2370948bf5c19860d03ce7495ee
EternitySyn <nicocarabella@hotmail.com>
2026-03-05 18:40:11 -0300

    add: vercel fix 3

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.html'],
-  build: {
-    target: 'es2015', // o 'es2015' si quieres compatibilidad más amplia
-  }
+  plugins: [react()]
 })
